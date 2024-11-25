60effcdbc749a3ea746d456c86f65e9a08f742ef
bmartinezproventum <bmartinez@proventum.cl>
2024-11-24 21:37:17 -0300

    arreglando dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,18 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
 import { AutenthicationService } from './services/autenthication.service'
 import { AuthService } from './services/auth.service';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
-  providers: [AuthService, AutenthicationService,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    EmailComposer // Add EmailComposer to providers
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()], providers: [AuthService, AutenthicationService,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        EmailComposer // Add EmailComposer to providers
+        , provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 
   constructor(public authenticationService: AutenthicationService) {
